8926a2146c0e9af4ed780bf97891954007e9f624
Tiquila McNeill <tiquilamceille@gmail.com>
2019-04-24 10:51:57 -0400

    nav

--- routing/routing-app/src/App.js
+++ routing/routing-app/src/App.js
@@ -6,6 +6,7 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
+import Navigation from "./components/Navigation"
 
 
 // const NewRoute= () => {
@@ -18,12 +19,14 @@ class App extends React.Component{
  render(){
    return(
      <BrowserRouter>
+     <Navigation>
      <Switch>
      <Route path ="/" component={Home} exact/>
      <Route path ="/about" component={About}/>
      <Route path ="/contact" component={Contact}/>
      <Route component={Error} />
      </Switch>
+     </Navigation>
      </BrowserRouter>
    )
   }
