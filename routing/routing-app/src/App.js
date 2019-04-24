bdfb5abcf0652d48e5c58483a6cff13761afddd4
Tiquila McNeill <tiquilamceille@gmail.com>
2019-04-24 10:42:03 -0400

    routing app 1 commit

---
+++ routing/routing-app/src/App.js
@@ -0,0 +1,56 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import{BrowserRouter, Route, Switch} from 'react-router-dom';
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
+
+
+// const NewRoute= () => {
+//   return(<div>
+//     New Route
+//   </div>);
+// }
+
+class App extends React.Component{
+ render(){
+   return(
+     <BrowserRouter>
+     <Switch>
+     <Route path ="/" component={Home} exact/>
+     <Route path ="/about" component={About}/>
+     <Route path ="/contact" component={Contact}/>
+     <Route component={Error} />
+     </Switch>
+     </BrowserRouter>
+   )
+  }
+}
+
+
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+export default App;
---
+++ routing/routing-app/src/components/Error.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Error = ()=> <div>Error: Path doesn't exist!</div>
+
+export default Error
