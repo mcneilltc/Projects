cc0d0c34a5f9557f9bb33c5b1c62a83c12bc57c8
Tiquila McNeill <tiquilamceille@gmail.com>
2019-04-30 11:26:00 -0400

    app

--- fireclassproj/fireclassproj/src/App.js
+++ fireclassproj/fireclassproj/src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import firebase from "firebase";
 
-function App() {
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      text: "default value"
+    };
+  }
+  render(){
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          {this.state.text}</h1>
+          <form onSubmit = "">
+          <input type= "text" name = "inputText" />
+          <input type ="submit" name = "submitButton" value  = "ok"/>
+          </form>
+       
       </header>
     </div>
   );
+}
 }
 
 export default App;
